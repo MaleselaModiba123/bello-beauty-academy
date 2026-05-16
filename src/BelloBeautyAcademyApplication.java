5f124c10695819f2065ddf65f22995ebcadc3a8f
Aaniquah Dicks <222641495@mycput.ac.za>
2026-05-16 20:46:23 +0200

    chore: Update pom.xml with Spring Boot and Mockito dependencies for Assignment 12 (#69)

--- src/BelloBeautyAcademyApplication.java
+++ src/BelloBeautyAcademyApplication.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class BelloBeautyAcademyApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelloBeautyAcademyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BelloBeautyAcademyApplication.class, args);
+    }
 }
