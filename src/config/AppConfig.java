941f8bb3fb2748d13216f8bfb4fa8c490a9a354f
Aaniquah Dicks <222641495@mycput.ac.za>
2026-05-17 15:31:48 +0200

    feat: Add AppConfig and OpenApiConfig Spring configuration classes (#69)

--- src/config/AppConfig.java
+++ src/config/AppConfig.java
@@ -1,4 +1,4 @@
-package api;
+package config;
 
 import factories.RepositoryFactory;
 import org.springframework.context.annotation.Bean;
--- src/config/OpenApiConfig.java
+++ src/config/OpenApiConfig.java
@@ -1,2 +1,20 @@
-package config;public class OpenApiConfig {
+package config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// configures the title and description shown at the top of the Swagger UI
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI belloBeautyOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Bello Beauty Academy API")
+                        .description("REST API for managing students, courses, and enrollments on the Bello Beauty Academy Platform.")
+                        .version("1.0.0"));
+    }
 }
